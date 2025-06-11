0710bcb16a084a5384d9e30eba6ca1969a9e8296
knewname <55625019+knewname@users.noreply.github.com>
2025-06-11 19:44:29 +0900

    add output format

--- Program.cs
+++ Program.cs
@@ -119,8 +119,7 @@ public class SlashModule : InteractionModuleBase<SocketInteractionContext>
         var storage = new GameRegisterStorage();
         var user = Context.User;
 
-        
-        await DeferAsync();
+        //await DeferAsync();
 
         // 메세지 ID를 미리 받기 위한 선 입력메세지 
         var embed = new EmbedBuilder()
@@ -128,21 +127,19 @@ public class SlashModule : InteractionModuleBase<SocketInteractionContext>
                 .WithDescription($"ID : [잠시 후 결정됨]\n모집인원수 : {max}\n시간 : {date} {time}\n 참여인원 : {user.Username}")
                 .WithColor(Color.Blue)
                 .Build();
+
         await RespondAsync(embed: embed);
 
-        //await DeferAsync(); // 슬래시 명령어 응답 딜레이 방지
-        //var botMessage = await Context.Interaction.GetOriginalResponseAsync();
-
-        // 메세지 ID 저장장
+        // 메세지 ID 저장
         var channel = Context.Channel as SocketTextChannel;
         var messages = await channel.GetMessagesAsync(1).FlattenAsync();
         var botMessage = messages.FirstOrDefault(msg => msg.Author.Id == Context.Client.CurrentUser.Id);
-        if (botMessage != null)
+        if (test != null)
         {
             // 메시지가 존재하면 이모지 반응 추가
-            await botMessage.AddReactionAsync(new Emoji("🆗"));
+            await test.AddReactionAsync(new Emoji("🆗"));
 
-            ulong messageId = botMessage.Id;
+            ulong messageId = test.Id;
             var msg = await Context.Channel.GetMessageAsync(messageId) as IUserMessage;
             
             // embed 포멧 실제 포멧으로 수정정
