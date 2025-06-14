1613b7f5e567b6cd6ca459ab1207e525a124d148
knewname <55625019+knewname@users.noreply.github.com>
2025-06-14 11:01:21 +0900

    add user function

--- GameRegister.cs
+++ GameRegister.cs
@@ -11,11 +11,11 @@ using System.Collections;
 
 public class GameRegisterInfo
 {
-    public string id { get; set; }         // messageId
+    public ulong id { get; set; }         // messageId
     public string date { get; set; }       // 등록날짜 (예: "2025-06-09")
     public string time { get; set; }       // 할 시간 (예: "21:00")
     public string game { get; set; }       // 게임 + 모드 (예: "LOL-Normal")
-    public List<string> users { get; set; } = new();       // 등록한 유저 이름 또는 ID
+    public List<ulong> users { get; set; } = new();       // 등록한 유저 이름 또는 ID
     public int cur { get; set; }           // 현재 등록된 명수
     public int max { get; set; }           // 최대 인원수
 }
@@ -28,7 +28,7 @@ public class GameRegisterStorage
         _filePath = filePath;
     }
 
-    // json 파일 저장 
+    // json 파일 저장
     public async Task SaveAsync(List<GameRegisterInfo> list)
     {
         var options = new JsonSerializerOptions
@@ -62,7 +62,7 @@ public class GameRegisterStorage
 
 
     // 인원 추가시 기존 json 파일 수정
-    public async Task<bool> AddUser(string msgId, string userName)
+    public async Task<bool> AddUser(ulong msgId, ulong userName)
     {
         var list = await LoadAsync();
 
@@ -100,7 +100,7 @@ public class GameRegisterStorage
 
 
     // 등록된 스케줄 json 저장
-    public async Task RegisterSchedule(string msgId, string date, string time, string game, string regUser, int max)
+    public async Task RegisterSchedule(ulong msgId, string date, string time, string game, ulong regUser, int max)
     {
         try
         {
